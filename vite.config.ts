cc701149feac32a44da35e5067427a1f63df4487
Dendi Renaldo Permana <dendi.renaldo@gmail.com>
2026-02-27 13:55:40 +0700

    [CHG] Environment Example

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     server: {
-      port: 3000,
+      port: JSON.stringify(env.FRONTEND_PORT),
       host: '0.0.0.0',
       proxy: {
         '/api': {
